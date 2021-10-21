4e3826f581a5cd6c7336a70af76eb2fb65f13d54
kingyeet9048 <sulaimanbada8048@gmail.com>
2021-10-21 17:45:26 -0500

    added system outs for better understanding.

--- src/main/java/cs485/preprocessing/DataCollector.java
+++ src/main/java/cs485/preprocessing/DataCollector.java
@@ -28,48 +28,51 @@ import com.google.gson.stream.JsonReader;
  */
 public class DataCollector {
 	private Map<String, List<Visit>> dowloadedData = new HashMap<>();
-
+	private String[] links; 
 	/**
 	 * 
 	 */
 	public DataCollector(String[] links) {
 		// TODO Auto-generated constructor stub
+		System.out.println("Pair each link to a hashmap entry");
 		for (String string : links) {
 			dowloadedData.put(string, null);
 		}
+		this.links = links;
 	}
 
 	public void fetchSaveData() throws IOException {
+		System.out.println("Starting Data Fetch...");
 		for (Map.Entry<String, List<Visit>> mapEntry : dowloadedData.entrySet()) {
 			String key = mapEntry.getKey();
-//			ZipFile val = entry.getValue();
-			
+			System.out.println(key.toString() + " is the first link");			
 			URL url = new URL(key);
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
 			InputStream in = connection.getInputStream();
 			ZipInputStream zipIn = new ZipInputStream(in);
 			ZipEntry entry = zipIn.getNextEntry();
+			System.out.println("Downloaded Zipped Folder...");
 			
 			Gson gson = new Gson();
 			while (entry != null) {
-
+				System.out.print("Unzipping and peaking folder --- ");
 				System.out.println(entry.getName());
 				if (!entry.isDirectory()) {
 					// if the entry is a file, extracts it
-					System.out.println("===File===");
+					System.out.println("No folder found. Found file instead. Opening Content...");
 					JsonReader jsonReader = new JsonReader(new InputStreamReader(zipIn));
 					JsonObject jsonObject = new JsonParser().parse(jsonReader).getAsJsonObject();
 					JsonElement eJsonElement = jsonObject.get("results");
+					System.out.println("Found the results element...");
 					List<Visit> visit = new ArrayList<>();
 					JsonArray jArray = eJsonElement.getAsJsonArray();
 					for (int i = 0; i < jArray.size(); i+=2) {
 						visit.add(gson.fromJson(jArray.get(i), Visit.class));
 					}
 					dowloadedData.put(key, visit);
+					System.out.println("Added and put visit list into hashmap. Will continue to next link if one exisits...");
 					
-				} else {
-					System.out.println("===Directory===");
 				}
 				zipIn.closeEntry();
 				entry = zipIn.getNextEntry();
@@ -79,6 +82,7 @@ public class DataCollector {
 			
 			
 		}
+		System.out.println("Finished with all the links. Printing first element content of hashmap: " + dowloadedData.get(links[0]).get(0).toString());
 	}
 	
 	public static void main (String args[]) {
